0b8cc5cfad3deb87239ecfcd6b5e829d6a281126
victory benson <victorybenson98@gmail.com>
2024-06-19 11:11:32 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -72,11 +72,11 @@ function App() {
 
   return (
     <div className=" flex items-center justify-center gap-8 min-h-screen">
-      <div className="space-y-2 flex items-center flex-col p-3">
+      <div className="space-y-2 flex items-center flex-col p-3">  
         <div className="font-bold text-xl text-center">{!isWinner && !isLoser && "Hangman Game!"}</div> 
         <div className="text-center text-lg py-2">
           {isWinner && <><strong className="text-green-500 text-3xl">You Won! </strong> <p onClick={()=> location.reload()} className="font-base bg-blue-400 text-white p-1 rounded my-2">Try again</p></>}
-          {isLoser && <><strong className="text-red-500 text-3xl">Nice Attempt! </strong> <p onClick={()=> location.reload()} className="font-base bg-blue-400 text-white p-1 rounded my-2">Try again</p></>}
+          {isLoser && <><strong className="text-red-500 text-3xl">Failed</strong> <p onClick={()=> location.reload()} className="font-base bg-blue-400 text-white p-1 rounded my-2">Try again</p></>}
         </div>
         <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
         <HangmanWord reveal={isLoser}  guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
