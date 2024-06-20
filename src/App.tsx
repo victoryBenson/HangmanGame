edc0329c5a8b0a2f79f41a0d4d0afa8c0c018c02
victory benson <victorybenson98@gmail.com>
2024-06-20 04:48:57 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
-import words from './wordList.json';
-import HangmanDrawing from "./components/HangmanDrawing";
-import HangmanWord from "./components/HangmanWord";
-import Keyboard from "./components/Keyboard";
-
-function getWord() {
-  return words[Math.floor(Math.random() * words.length)]
-}
-
-function App() {
-  const [wordToGuess, setWordToGuess] = useState(getWord)
-
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
-
-  const incorrectLetters = guessedLetters.filter(
-      letter => !wordToGuess.includes(letter)
-    );
-
-  const isLoser = incorrectLetters.length >= 6
-  const isWinner = wordToGuess
-    .split("")
-    .every(letter => guessedLetters.includes(letter))
-
-  const addGuessedLetter = useCallback(
-    (letter: string) => {
-
-        if (guessedLetters.includes(letter) || isLoser || isWinner) return
-    
-        setGuessedLetters(currentLetters  => [...currentLetters, letter])
-        
-      },
-      [guessedLetters, isLoser, isWinner]
-  )
-  
-   
-  //check if you clicked any button
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      const key = e.key
-      if (!key.match(/^[a-z]$/)) return
-
-      e.preventDefault()
-      addGuessedLetter(key)
-    }
-
-    document.addEventListener("keypress", handler)
-
-    return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [guessedLetters])
+import { Route, BrowserRouter, Routes,} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import StartGame from "./pages/StartGame"
+import Notfound from "./pages/Notfound"
 
 
-  //click enter button to refresh the game!
-  useEffect(() => {
-    const handler = (e: KeyboardEvent) => {
-      const key = e.key
-      if (key !== "Enter") return
-
-      e.preventDefault()
-      setGuessedLetters([])
-      setWordToGuess(getWord())
-    }
-
-    document.addEventListener("keypress", handler)
-
-    return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [])
-
+const App: React.FC = () => {
   return (
-    <div className=" flex items-center justify-center gap-8 min-h-screen">
-      <div className="space-y-2 flex items-center flex-col p-3">  
-        <div className="font-bold text-xl text-center">{!isWinner && !isLoser && "Hangman Game!"}</div> 
-        <div className="text-center text-lg py-2">
-          {isWinner && <><strong className="text-green-500 text-3xl">You Won! </strong> <p onClick={()=> location.reload()} className="font-base bg-blue-400 text-white p-1 rounded my-2">Play again</p></>}
-          {isLoser && <><strong className="text-red-500 text-3xl">Failed</strong> <p onClick={()=> location.reload()} className="font-base bg-blue-400 text-white p-1 rounded my-2">Try again</p></>}
-        </div>
-        <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
-        <HangmanWord reveal={isLoser}  guessedLetters={guessedLetters} wordToGuess={wordToGuess}/>
-        <div className="self-stretch">
-            <Keyboard 
-              disabled = {isWinner || isLoser}
-              activeLetters = {guessedLetters.filter(letter => (
-                wordToGuess.includes(letter)
-              ))}
-              inactiveLetters = {incorrectLetters}
-              addGuessedLetter = {addGuessedLetter}
-            />
-        </div>
-        <div>
-          <strong>Hint:</strong> Find the word that will match the empty space above
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" index element={<HomePage/>}/>
+            <Route path="/startgame" element={<StartGame/>}/>
+            <Route path="*" element={<Notfound/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,10 @@
+
+const Notfound = ():JSX.Element =>  {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+        <p> Page Not Found</p>
+    </div>
+  )
+}
+
+export default Notfound
